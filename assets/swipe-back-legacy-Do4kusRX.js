64fbda6bd69783f495ae7002af1054e4b7305051
saraum01 <saraummariel1@gmail.com>
2025-02-20 15:46:34 +0800

    Updates

--- assets/swipe-back-legacy-Do4kusRX.js
+++ assets/swipe-back-legacy-Do4kusRX.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BodOkdrn.js","./index3-legacy-BKDfgvqW.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-DDMl1OLy.js","./index3-legacy-vvSVsb24.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
