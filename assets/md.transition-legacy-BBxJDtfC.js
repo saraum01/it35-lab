c60312da8ccc2b33a61d1849c14c3e108fc6fc33
saraum01 <saraummariel1@gmail.com>
2025-03-05 10:43:42 -0400

    Updates

--- assets/md.transition-legacy-BBxJDtfC.js
+++ assets/md.transition-legacy-BBxJDtfC.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CZjXK8T8.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
+System.register(["./index-legacy-BZR-9lUH.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
