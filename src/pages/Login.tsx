aefd8b9fd2bf1a93b13ebf34eebf9cc63046d669
saraum01 <saraummariel1@gmail.com>
2025-03-07 22:10:53 +0800

    Added new features in login.tsx and updating my vite.config.ts

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,21 +1,28 @@
 import {
+  IonAvatar,
   IonButton,
   IonButtons,
-  IonContent,
-  IonHeader,
-  IonMenuButton,
-  IonPage,
-  IonTitle,
-  IonToolbar,
+  IonCheckbox,
+  IonContent, 
+  IonHeader, 
+  IonIcon, 
+  IonInput, 
+  IonInputPasswordToggle, 
+  IonItem, 
+  IonMenuButton, 
+  IonPage, 
+  IonTitle, 
+  IonToolbar, 
   useIonRouter
 } from '@ionic/react';
+import { logoIonic } from 'ionicons/icons';
 
 function Login() {
   const navigation = useIonRouter();
-  
   const doLogin = () => {
     navigation.push('/it35-lab/app', 'forward', 'replace');
   };
+
   return (
     <IonPage>
       <IonHeader>
@@ -27,7 +34,26 @@ function Login() {
         </IonToolbar>
       </IonHeader>
       <IonContent className='ion-padding'>
-        <IonButton onClick={() => doLogin()} expand="full">
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+          <IonAvatar>
+            <img alt="Silhouette of a person's head" src="https://ionicframework.com/docs/img/demos/avatar.svg" />
+          </IonAvatar>
+        </div>
+        
+        <IonItem>
+          <IonInput label="Username:" placeholder="Username" />
+        </IonItem>
+        <IonItem>
+          <IonInput type="password" label="Password:" placeholder="Password">
+            <IonInputPasswordToggle slot="end" />
+          </IonInput>
+        </IonItem>
+        
+        <IonItem>
+          <IonCheckbox>I agree to the terms and conditions</IonCheckbox>
+        </IonItem>
+        
+        <IonButton onClick={doLogin} expand="full">
           Login
         </IonButton>
       </IonContent>
