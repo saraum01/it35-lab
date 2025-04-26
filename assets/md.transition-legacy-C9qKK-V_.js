5407623eea4abeca796a4d9a53e4f310a2501564
saraum01 <saraum01@gmail.com>
2025-04-26 01:11:15 -0400

    Updates

--- assets/md.transition-legacy-C9qKK-V_.js
+++ assets/md.transition-legacy-C9qKK-V_.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CvbxrsXe.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-DEy9v3QS.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/status-tap-legacy-8VZ2W2Bf.js
+++ assets/status-tap-legacy-8VZ2W2Bf.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CvbxrsXe.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
+System.register(["./index-legacy-DEy9v3QS.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
